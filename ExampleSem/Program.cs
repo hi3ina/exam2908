1b26e1745ce2707bb25558ebcde32dd6965fa921
hi3ina <hi3ina@gmail.com>
2022-09-07 21:04:18 +0300

    создали гит игнор в папке exampelesem

---
+++ ExampleSem/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number = Convert.ToInt32(Console.ReadLine()); // конвертация в число.
+
+int result = number * number;
+
+Console.WriteLine(result);
