86acf27e106d5539c20819b42c555d7b6da097a6
hi3ina <hi3ina@gmail.com>
2022-09-08 22:06:03 +0300

    случ число из отрезка и показт макс цифру

--- ExampleSem2/Program.cs
+++ ExampleSem2/Program.cs
@@ -3,10 +3,18 @@
 // напишите программу которая выводит случайное число из отрезкеа 10 99 и показывает наибольшую цифру числа
 // 78 -> 8; 12 -> 2; 85 -> 8
 Random random = new Random(); // для генерации рандомных\случайных чисел
-
-int random1 = random.Next(10, 100); //вызываем одно случайное число
-int random2 = random.Next(10, 100); //вызываем второе случайное число
-
+//string str = Console.ReadLine(); //этот метод возвращает стоку
+int random1 = random.Next(10, 100); //вызываем одно случайное число, возвращает число//int random2 = random.Next(10, 100); //вызываем второе случайное число
+// int random2 = random.Next(10,100); //вызываем второе случайное число, возвращает число//int random2 = random.Next(10, 100); //вызываем второе случайное число
 Console.WriteLine(random1);
-
-Console.WriteLine(random2);
+// Console.WriteLine(random2);
+int lastNumber = random1 % 10; // находим остаток т е вторую цифру двухзначного числа
+int firstNumber = random1 / 10; // находим десятки  т е первую цифру двухзначного числа
+if (lastNumber > firstNumber) // находим два последних числа random1 % 100 и т д
+{
+    Console.WriteLine(lastNumber);
+}
+else
+{
+    Console.WriteLine(firstNumber);
+}
