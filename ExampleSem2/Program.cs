9942e51fd57c73997fcd0084ec42178ba8d8ff35
hi3ina <hi3ina@gmail.com>
2022-09-08 21:47:55 +0300

    генерациz рандомных\случайных чисел

--- ExampleSem2/Program.cs
+++ ExampleSem2/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+// напишите программу которая выводит случайное число из отрезкеа 10 99 и показывает наибольшую цифру числа
+// 78 -> 8; 12 -> 2; 85 -> 8
+Random random = new Random(); // для генерации рандомных\случайных чисел
+
+int random1 = random.Next(10, 100); //вызываем одно случайное число
+int random2 = random.Next(10, 100); //вызываем второе случайное число
+
+Console.WriteLine(random1);
+
+Console.WriteLine(random2);
