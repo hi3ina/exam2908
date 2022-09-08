c24ddbe522e81f0e0f87d3024da0458b4ca3ad2e
hi3ina <hi3ina@gmail.com>
2022-09-08 11:36:15 +0300

    ЗЯП 1 семенар  дз1 задача 4

--- ExampleSem/Program.cs
+++ ExampleSem/Program.cs
@@ -20,14 +20,37 @@
 
 // Console.WriteLine(max);
 
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
 
+// int a = Convert.ToInt32(Console.ReadLine());//ЗЯП 1 семенар  дз1 задача 2
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+// {
+//     if (a > b) max = a;
+
+//     else max = b;
+// }
+// Console.Write("max=");
+// Console.WriteLine(max);
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a = Convert.ToInt32(Console.ReadLine());//ЗЯП 1 семенар  дз1 задача 4
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 {
-    if (a > b) max = a;
+    if (max > b) max = max;
 
     else max = b;
+}
+{
+    if (max > c) max = max;
+
+    else max = c;
 }
 Console.Write("max=");
 Console.WriteLine(max);
