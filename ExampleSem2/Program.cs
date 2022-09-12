1121f14a14c8d7255676ff758f8f8ebc7bb1cd0e
hi3ina <hi3ina@gmail.com>
2022-09-12 15:54:56 +0300

    ничего не получилось пока

--- ExampleSem2/Program.cs
+++ ExampleSem2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-// напишите программу которая выводит случайное число из отрезкеа 10 99 и показывает наибольшую цифру числа
+// напишите программу которая выводит случайное число из отрезке 10 99 и показывает наибольшую цифру числа
 // 78 -> 8; 12 -> 2; 85 -> 8
 // Random random = new Random(); // для генерации рандомных\случайных чисел
 // //string str = Console.ReadLine(); //этот метод возвращает стоку
@@ -28,7 +28,7 @@
 // int a = random.Next(99, 1000);
 // int a = Convert.ToInt32(Console.ReadLine());
 
-// Random random = new Random(); //вводим случайное трехзначное число]
+// Random random = new Random(); //вводим случайное трехзначное число] //задача 10 д з 2
 // int random1 = random.Next(99, 1000);
 
 // Console.WriteLine(random1);
@@ -59,3 +59,26 @@
 // }
 // 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
 
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int number3 = a % 10;
+
+// if (a < 99)
+// {
+//     if (a > -99)
+//     {
+//         Console.WriteLine($"Третьей цифры в числе {a} нет");
+
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Третья цифра числа {a} - {number3}");
+//     }
+// }
+// Console.WriteLine($"Третья цифра числа {a} - {number3}");
+// return;
+// Math.Round(num, 1)// число и один знак после запятой
+// Console.WriteLine("Введите число: ");
+// // string str = Console.ReadLine();
+// Console.WriteLine("Третья цифра числа -");
