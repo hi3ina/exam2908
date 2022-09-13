0c3bb0269b7a6e366b569692753a76f7972a2f34
hi3ina <hi3ina@gmail.com>
2022-09-13 21:54:20 +0300

    решение задачи 21 сем 3 д з 3

--- ExampleSem3/Program.cs
+++ ExampleSem3/Program.cs
@@ -38,88 +38,83 @@
 // Задача 19 Напишите программу, которая принимает на вход пятизначное число 
 // и проверяет, является ли оно палиндромом.
 
-Console.Write("Введите пятизначное число: ");
-bool number = int.TryParse(Console.ReadLine(), out int M);
+// Console.Write("Введите пятизначное число: ");
+// bool number = int.TryParse(Console.ReadLine(), out int M);
 
-bool IsValidate(bool number) //проверка на не число
-{
-    if (number != true)
-    {
-        Console.WriteLine("Некорректное число");
-        return false;
-    }
-    if (M < -9999 || M > -99999)// проверяем что б число было пятизначным
-    {
-        int minus = M * -1; //избавляемся от минуса
-        return true;
-    }
-    if (M < 10000 || M > 99999) // проверяем что б число было пятизначным
-    {
-        Console.WriteLine("Некорректное число");
-        return false;
-    }
-    return true;
-}
-bool result = IsValidate(number);
-if (!result)
-{
-    return;
-}
-bool Palindrom(bool number)
-{
-    int m1 = M / 10000; // разбираем число на цифры
-    int m2 = M / 1000;
-    int m4 = M % 100;
-    int m5 = M % 10;
-    if (m1 == m5 && m2 == m4)
-    {
-        Console.WriteLine($"Число {M} является Палиндромом");
-        return true;
-    }
+// bool IsValidate(bool number) //проверка на не число
+// {
+//     if (number != true)
+//     {
+//         Console.WriteLine("Некорректное число");
+//         return false;
+//     }
+//     if (M < -9999 || M > -99999)// проверяем что б число было пятизначным
+//     {
+//         int minus = M * -1; //избавляемся от минуса
+//         return true;
+//     }
+//     if (M < 10000 || M > 99999) // проверяем что б число было пятизначным
+//     {
+//         Console.WriteLine("Некорректное число");
+//         return false;
+//     }
+//     return true;
+// }
+// bool result = IsValidate(number);
+// if (!result)
+// {
+//     return;
+// }
+// bool Palindrom(bool number)
+// {
+//     int m1 = M / 10000; // разбираем число на цифры
+//     int m2 = M / 1000;
+//     int m4 = M % 100;
+//     int m5 = M % 10;
+//     if (m1 == m5 && m2 == m4)
+//     {
+//         Console.WriteLine($"Число {M} является Палиндромом");
+//         return true;
+//     }
 
-    Console.WriteLine($"Число {M} не является Палиндромом");
-    return false;
-}
-Palindrom(number);
+//     Console.WriteLine($"Число {M} не является Палиндромом");
+//     return false;
+// }
+// Palindrom(number);
 
 // Задача 21 Напишите программу, которая принимает на вход координаты двух точек
 //  и находит расстояние между ними в 3D пространстве.
 // формулу для нахождения расстояния между точками в пространстве
 // |AB| = сумма квадратов под корнем (Xb -Xa)кв+(Yb -Ya)кв+(Zb -Za)кв
 
-// Console.WriteLine("Введите координаты точки a: Xa  ");
-// bool isNumberXa = double.TryParse(Console.ReadLine(), out double Xa);
+Console.Write("Введите координаты точки a: Xa  ");
+bool isNumberXa = int.TryParse(Console.ReadLine(), out int Xa);
+Console.Write("Ya  ");
+bool isNumberYa = int.TryParse(Console.ReadLine(), out int Ya);
+Console.Write("Za  ");
+bool isNumberZa = int.TryParse(Console.ReadLine(), out int Za);
 
-// Console.WriteLine("Ya  ");
-// bool isNumberYa = double.TryParse(Console.ReadLine(), out double Ya);
+if (!isNumberXa || !isNumberYa || !isNumberZa)
+{
+    Console.WriteLine("Числа введены не верно.");
+    return;
+}
+Console.Write("Введите координаты точки b: Xb  ");
+bool isNumberXb = int.TryParse(Console.ReadLine(), out int Xb);
+Console.Write("Yb  ");
+bool isNumberYb = int.TryParse(Console.ReadLine(), out int Yb);
+Console.Write("Zb  ");
+bool isNumberZb = int.TryParse(Console.ReadLine(), out int Zb);
 
-// Console.WriteLine("Za  ");
-// bool isNumberZa = double.TryParse(Console.ReadLine(), out double Za);
+if (!isNumberXb || !isNumberYb || !isNumberZb)
+{
+    Console.WriteLine("Числа введены не верно.");
+    return;
+}
+Console.Write($"Расстояние между точками a и b равно: {GetLenght(Xa, Ya, Za, Xb, Yb, Zb)}");
 
-// if (isNumberXa != true || isNumberYa != true || isNumberZa != true)
-// {
-//     Console.WriteLine("Числа введены не верно.");
-//     return;
-// }
-
-// Console.WriteLine("Введите координаты точки b: Xb  ");
-// bool isNumberXb = double.TryParse(Console.ReadLine(), out double Xb);
-
-// Console.WriteLine("Yb  ");
-// bool isNumberYb = double.TryParse(Console.ReadLine(), out double Yb);
-
-// Console.WriteLine("Zb  ");
-// bool isNumberZb = double.TryParse(Console.ReadLine(), out double Zb);
-
-// if (isNumberXb != true || isNumberYb != true || isNumberZb != true)
-// {
-//     Console.WriteLine("Числа введены не верно.");
-//     return;
-// }
-
-// double Method(double Xa, double Ya, double Za, double Xb, double Yb, double Zb)
-// {
-//     double ab = Math.Sqrt((Math.Pow(Xb - Xa), 2) + (Math.Pow(Yb - Ya), 2) + (Math.Pow(Zb - Za), 2));
-//     return ab;
-// }
-// Console.WriteLine($"Расстояние между точками a и b равно: {Method(Xa, Ya, Za, Xb, Yb, Zb)}");
+double GetLenght(int Xa, int Ya, int Za, int Xb, int Yb, int Zb)
+{
+    return Math.Sqrt(Math.Pow((Xb - Xa), 2) + Math.Pow((Yb - Ya), 2) + Math.Pow((Zb - Za), 2));
+    // return ab;
+}
