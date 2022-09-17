f02e7476c52b5b05582578d9c1b8c4bd63340f9b
hi3ina <hi3ina@gmail.com>
2022-09-17 18:18:26 +0300

    решение 29 задачи

--- ExampleSem4/Program.cs
+++ ExampleSem4/Program.cs
@@ -1,8 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
-// и выводит их на экран.
+﻿
+#nullable disable
+Console.WriteLine("Enter length - ");
 
+bool isNumber = int.TryParse(Console.ReadLine(), out int lenght);
+if (!isNumber || lenght <= 0)
+{
+    Console.WriteLine("Invalid number");
+    return;
+}
+
+int[] result = FillArray(lenght);
+PrintArray(result);
+
+void PrintArray(int[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.Write("Числа введены неверно");
+        return;
+    }
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int[] FillArray(int lenght)
+{
+    int[] array = new int[lenght];
+
+    string input = Console.ReadLine();
+
+    string[] inputArray = input.Split(",");
+    if (inputArray.Length != lenght)
+    {
+        return new int[0];
+    }
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        array[i] = Convert.ToInt32(inputArray[i]);
+    }
+    return array;
+}
+// Console.WriteLine("Введите 8 чисел:");
+// bool isNumber = int.TryParse(Console.ReadLine(), out int lenght);
+// if (!isNumber || lenght <= 0)
+// {
+//     Console.WriteLine("Это не число");
+//     return;
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
 
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
 // и возводит число A в натуральную степень B.
@@ -10,3 +64,5 @@
 // Задача 27: Напишите программу, которая принимает на вход число
 //  и выдаёт сумму цифр в числе.
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
