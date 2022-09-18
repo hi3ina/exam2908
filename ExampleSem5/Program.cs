bcee247b45ca7096cbbcdc0b60413d0b97658f9c
hi3ina <hi3ina@gmail.com>
2022-09-19 00:36:51 +0300

    ничего не получилось зад 36

--- ExampleSem5/Program.cs
+++ ExampleSem5/Program.cs
@@ -1,14 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 #nullable disable
-// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
+// Console.WriteLine("Введите размер массива: ");
+
+// bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+// if (!isNumber || length <= 0)
+// {
+//     Console.WriteLine("Неверный ввод");
+//     return; // проверка на число
+// }
+
+// // int[] result = FillArray(length);
+// // PrintArray(result);
+// // {
+// int[] array = new int[length];
+// array[length] = Random.Next(99, 1000);
+// PrintArray(result);
+
+// // int[] result = FillArray(length);
+// // void PrintArray(int[] array);
+// Console.WriteLine($"{array}");
+// }
+// Random random = new Random();
+// int random1 = random.Next(99, 1000);
+// int result = a % 2;
+// if (result == 0)
+// {
+//     return result;
+// }
+// else
+// {
+//     return false;
+// }
+
+// Console.Write($"Количество чётных чисел в массиве - {result}");
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
 
+// Console.WriteLine("Введите размер массива: ");
+
+// bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+// if (!isNumber || length <= 0)
+// {
+//     Console.WriteLine("Неверный ввод");// проверка на число
+// }
+// Random random = new Random();
+// int[] array = new int[length]; //задали условия массива случайные числа в количестве length
+// // void PrintArray (int[] array);
+// // Console.WriteLine($"{array}");// вывели полученный массив на печать
+
+// int result = 0;//задали переменную результата
+// {
+//     for (int i = 1; i < array.Length - 1; i += 2)// перебираем все нечетные индексы с первого через один
+//     {
+//         result++; //прибавляем к результату каждый новый найденный, количество увеличивается
+//     }
+// }
+// Console.Write($"Сумма элементов нечетных индексов равна {result}");//ответ
 
 // Задача 38: Задайте массив вещественных чисел.
 // Найдите разницу между максимальным и минимальным элементами массива.
 
+
+// if (a > b) max = a;// метод GetMaxNumber находит максимальное число
+
+// else max = b;
+
+// if (a < b) min = a; //метод GetMinNumber находит минимальное число?
+
+// else min = b;
+
+
+// Console.Write($"{max} - {min} = {max - min}");
