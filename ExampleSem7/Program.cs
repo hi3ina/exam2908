8e76663f6be6d5c9dca6763a43e91af219f70ba9
hi3ina <hi3ina@gmail.com>
2022-09-21 00:12:08 +0300

    пока не решила

--- ExampleSem7/Program.cs
+++ ExampleSem7/Program.cs
@@ -17,7 +17,7 @@
 //     Console.WriteLine("Неверный ввод");//задали размер проверили на число
 //     return;
 // }
-// double[,] mn = new double[m, n];
+// double[,] mn = new double[m, n];// double массив вещественных чисел
 // void CreateRandomDoubleArray(int m, int n)
 // {
 //     Random random = new Random();
@@ -43,18 +43,143 @@
 // CreateRandomDoubleArray(m, n);
 // Print2DDoubleArray(mn);
 //
-//
-// Задача 50. Напишите программу, которая на вход принимает число и ищет в двумерном массиве, Задача 50. 
+//Задача 50. Напишите программу, которая на вход принимает число и ищет в двумерном массиве, Задача 50. 
 //и возвращает индексы этого элемента или же указание, что такого элемента нет.
 //
-//
+// Console.WriteLine("Задайте число строк m= ");
+// bool isNumberm = int.TryParse(Console.ReadLine(), out int m);
+// if (!isNumberm || m <= 1)
+// {
+//     Console.WriteLine("Неверный ввод");
+//     return;
+// }
+// Console.WriteLine("Задайте число столбцов n= ");
+// bool isNumbern = int.TryParse(Console.ReadLine(), out int n);
+// if (!isNumbern || n <= 1)
+// {
+//     Console.WriteLine("Неверный ввод");//задали размер проверили на число
+//     return;
+// }
+// int[,] CreateRandomIntArray(int m, int n)// int массив
+// {
+//     int[,] array = new int[m, n];
+//     Random random = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = random.Next(10, 10);
+//         }
+//     }
+//     return array;
+// }
+// void Print2DIntArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($" {array[i, j]}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int[,] array2 = CreateRandomIntArray(m, n);
+// Print2DIntArray(array2);
 
-//
-//
+// Console.WriteLine("Что ищем?");
+// bool isNumber = int.TryParse(Console.ReadLine(), out int number);// проверка на целое число
+// if (!isNumber)
+// {
+//     Console.WriteLine("Это не число");
+//     return;
+// }
+// string GuessArrayNumber(int[,] array, int number)
+// {
+//     string someString = "";
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j] == number)
+//             {
+//                 someString = $"Найдено {i}, {j}";
+//                 break;
+//             }
+//             else
+//             {
+//                 someString = "Не найдено";
+//             }
+//         }
+//     }
+//     return someString;
+// }
+// string cherstr2 = GuessArrayNumber(array2, number);
+// Console.WriteLine(cherstr2);
 //
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 //
-//
-//
+Console.WriteLine("Задайте число строк m= ");
+bool isNumberm = int.TryParse(Console.ReadLine(), out int m);
+if (!isNumberm || m <= 1)
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+Console.WriteLine("Задайте число столбцов n= ");
+bool isNumbern = int.TryParse(Console.ReadLine(), out int n);
+if (!isNumbern || n <= 1)
+{
+    Console.WriteLine("Неверный ввод");//задали размер проверили на число
+    return;
+}
+int[,] CreateRandomIntArray(int m, int n)// int массив
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(10);
+        }
+    }
+    return array;
+}
+void Print2DIntArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array2 = CreateRandomIntArray(m, n);
+Print2DIntArray(array2);
 
-
+void PrintDoubleArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} | ");
+    }
+}
+double[] NumberRow(int[,] array)
+{
+    double[] numberRow = new double[n];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            numberRow[j] = numberRow[j] + array[i, j];
+        }
+        numberRow[j] = numberRow[j] / array.GetLength(0);
+    }
+    return numberRow;
+}
+double[] numberRowDouble = NumberRow(array2);
+Console.WriteLine();
+PrintDoubleArray(numberRowDouble);
