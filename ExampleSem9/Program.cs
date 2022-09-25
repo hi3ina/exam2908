c066f252ef37f6150ad65b77a599177ca0b9cb77
hi3ina <hi3ina@gmail.com>
2022-09-25 20:09:48 +0300

    задача 66

--- ExampleSem9/Program.cs
+++ ExampleSem9/Program.cs
@@ -7,12 +7,52 @@
 
 // M = 4; N = 8. -> ""4, 6, 7, 8""
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите m = ");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n = ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+if (!isNumberN || !isNumberM || n < m)
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+void Recursive(int m, int n)
+{
+    if (n - m == -1) return;
+    Console.WriteLine(m);
+    Recursive(m + 1, n);
+}
+Console.WriteLine();
+Recursive(m, n);
 
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N. M = 1; N = 15 -> 120  M = 4; N = 8. -> 30
 
-// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int SumOfNumbers(int m, int n)
+{
+    if (n - m == -1) return 0;
+    return m + SumOfNumbers(m + 1, n);
+}
+Console.WriteLine(SumOfNumbers(m, n));
 
-// m = 2, n = 3 -> A(m,n) = 29
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n. m = 2, n = 3 -> A(m,n) = 29
 
+
+long AckermanFunction(int m, long n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return AckermanFunction(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+    else return n + 1;
+}
+Console.WriteLine(AckermanFunction(m, n));
